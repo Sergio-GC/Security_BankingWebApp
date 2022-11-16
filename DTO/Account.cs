75c82d2122b61599075aa2a336bc382295a49662
Sergio <gomez.c.sergio@outlook.com>
2022-11-16 18:49:49 +0100

    Add project files.

---
+++ DTO/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Account
+    {
+        public int accountNumber { get; set; } // primary key
+        public string accountNbOwner { get; set; } // foreign key
+        public string type { get; set; }
+        public float amount { get; set; }
+    }
+}
---
+++ DTO/User.cs
@@ -0,0 +1,12 @@
+﻿namespace DTO
+{
+    public class User
+    {
+        public string accountNb { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string salt { get; set; }
+        public string password { get; set; }
+        public List<Account> accounts { get; set; }
+    }
+}
