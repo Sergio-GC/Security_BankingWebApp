066643643077e62e4b5124d16df4078e1fa79ca1
Sergio <gomez.c.sergio@outlook.com>
2022-11-20 18:28:33 +0100

    Add LogIn controller

---
+++ BankingWebApp/Controllers/LogInController.cs
@@ -0,0 +1,43 @@
+﻿using BankingWebApp.Models;
+using BLL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingWebApp.Controllers
+{
+    public class LogInController : Controller
+    {
+
+        private IUserManager userManager { get; }
+
+        public LogInController(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        // GET: LogInController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: LogInController/Index
+        [HttpPost]
+        public ActionResult Index(LoginVM loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userManager.getUserByLogin(loginVM.accountNb, loginVM.password);
+
+                if(user != null)
+                {
+                    HttpContext.Session.SetString("userId", user.accountNb);
+                    return RedirectToAction("Index", "Accounts");
+                }
+            }
+
+            ModelState.AddModelError("", "Could not find an account with the provided password.");
+            return View(loginVM);
+        }
+    }
+}
---
+++ BankingWebApp/Models/LoginVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankingWebApp.Models
+{
+    public class LoginVM
+    {
+        [Required]
+        public string accountNb { get; set; }
+        [Required]
+        public string password { get; set; }
+    }
+}
